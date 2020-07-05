6e86bd88338069fb3262f5e4bd842dab9fe2bee3
AriixIT <49060231+AriixIT@users.noreply.github.com>
2020-07-05 14:51:29 +0200

    created functions in app.js

--- webroot/assets/js/app.js
+++ webroot/assets/js/app.js
@@ -1,1 +1,15 @@
+const baseurl = "http://www.pearcenet.ch:8080";
 
+const getCourse = (id) => {
+    url = "";
+    id ? url = "/getcourse" : url = "/getcourse?id=" + id;
+    fetch(baseurl + url)
+    .then(response => response.json())
+    .then(data => {return data});
+}
+
+const createCourse = (course) => {
+    fetch(baseurl + "/createcourse")
+    .then(response => response.json())
+    .then(data => {return data});
+}
