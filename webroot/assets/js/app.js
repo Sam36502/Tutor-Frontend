47bd6834702de9fc1aaa83fb78b59587464f015b
AriixIT <49060231+AriixIT@users.noreply.github.com>
2020-07-05 18:16:04 +0200

    added create form

--- webroot/assets/js/app.js
+++ webroot/assets/js/app.js
@@ -2,16 +2,26 @@ const baseurl = "http://www.pearcenet.ch:8080";
 
 const getCourse = (id) => {
     url = "";
-    id ? url = "/getcourse?id=" + id : url = "/getcourse";
-    fetch(baseurl + url)
-    .then(response => response.json())
-    .then(data => {return data});
+    id ? url = "/getCourse?id=" + id : url = "/getCourse";
+    let request = new XMLHttpRequest();
+
+    request.open('GET', baseurl + url, true);
+    request.onload = function () {
+        let data = JSON.parse(this.response);
+        console.log(data);
+    };
+    request.send();
 }
 
 const createCourse = (course) => {
-    fetch(baseurl + "/createcourse")
+    fetch(baseurl + "/createCourse", {
+        method: "POST",
+        body: course
+    })
     .then(response => response.json())
-    .then(data => {return data});
+    .then(json => {
+      console.log('parsed json', json)
+    })
 }
 
 const loadCourses = () => {
@@ -19,8 +29,26 @@ const loadCourses = () => {
 
     const template = document.getElementById("tutorial");
     const tutorialList = document.getElementById("tutorialList");
+    data = [
+        {
+            title: "How to make fake data",
+            description: "Here you can learn how to make fake data",
+            author: "Amin"
+        },
+        {
+            title: "How to fuck up at tbz",
+            description: "Here you can learn how to fuck up at tbz",
+            author: "Julian"
+        },
+        {
+            title: "How to die",
+            description: "Here you can learn how to die",
+            author: "Sam"
+        },
+    ]
 
     const tutorials = Mustache.render(template.innerHTML, {'tutorials': data})
+
     tutorialList.innerHTML = tutorials;
 }
 
